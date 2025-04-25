4243ad3a6d8038ed4d142a12adf58af3389511c8
Alina Dakhno <alinad@hexaware.com>
2025-04-25 13:42:10 +0200

    mongoDB, connect mongoose, create models

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
 import express from "express";
-import products from "./data/products.js";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 dotenv.config();
+
 const port = process.env.PORT || 4000;
 
 const app = express();
-
+connectDB();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
+// Error handling middleware
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => {
-  console.log(`sever running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <main className="py-3">
+      <main>
         <Container>
           <Outlet />
         </Container>
